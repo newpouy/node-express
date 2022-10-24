76fad28b69f63700e1c9607a3518bfff13652288
kim noel <kimnoel@kimui-MacBookPro.local>
2022-10-24 21:11:54 +0900

    khk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   res.sendStatus(200);
+});
+
+app.get('/khk', (req, res) => {
+  res.send('어서오세요 김회경서버입니다!');
 });
 
 app.listen(port, () => {
