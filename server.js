cf7c98fa4cd4a25212eea766e5e9d8ffcfd9e0ac
kim noel <kimnoel@kimui-MacBookPro.local>
2022-10-24 21:39:53 +0900

    fix

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.get('/health', (req, res) => {
 
 app.get('/kth', (req, res) => {
   res.send('Hello from the Tae hee server!');
-
+})
 app.get('/vincent', (req, res) => {
   res.send('Hello this is Vincent from the server! 가즈아~~~');
 });
 
 app.get('/angel', (req, res) => {
   res.send('승범 엔젤서버입니다!');
-
+})
 app.get('/blossom', (req, res) => {
   res.send('Hello blossom server!');
-
+})
 app.get('/khk', (req, res) => {
   res.send('어서오세요 김회경서버입니다!');
 });
@@ -30,4 +30,3 @@ app.get('/khk', (req, res) => {
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
-
